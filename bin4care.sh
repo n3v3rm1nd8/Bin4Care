c5ad6ec94575db99481ebbb23da36f33e67b0631
n3v3rm1nd8 <130241238+n3v3rm1nd8@users.noreply.github.com>
2023-07-06 05:34:41 +0200

    Update bin4care.sh

--- bin4care.sh
+++ bin4care.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export valueID=`/usr/bin/id -u`
-export filterSUID=`/usr/bin/find / -perm -4000 -user root 2>/dev/null | /usr/bin/awk -F '/' '{print $NF}' | /usr/bin/tr '\n' '|' | /usr/bin/sed 's/|$//' | /usr/bin/sed 's/\([^|]\+\)/^\1$/g'`
+export filterBinary=`/usr/bin/find / -perm -4000 -user root 2>/dev/null | /usr/bin/awk -F '/' '{print $NF}' | /usr/bin/tr '\n' '|' | /usr/bin/sed 's/|$//' | /usr/bin/sed 's/\([^|]\+\)/^\1$/g'`
+export filterCapabilitie=`/usr/sbin/getcap -r / 2>/dev/null | /usr/bin/awk -F ' ' '{print $1}' | awk -F '/' '{print $NF}' | /usr/bin/tr '\n' '|' | /usr/bin/sed 's/|$//' | /usr/bin/sed 's/\([^|]\+\)/^\1$/g'`
 echo " ____  _       _  _    ____
 | __ )(_)_ __ | || |  / ___|__ _ _ __ ___
 |  _ \| | '_ \| || |_| |   / _\` | '__/ _ \\
@@ -25,9 +26,15 @@ then
                 echo -e "\n${red}[!] This script needs to be run as a privileged user${end}"
                 echo
 else
-                echo -e "\n${blue}[*] The possible exploitable binaries for privilege escalation that you have are:${end}"
-                for data in $(/usr/bin/curl -s -X GET "https://gtfobins.github.io/" | /usr/bin/html2text | /usr/bin/awk '{print$1}' | /usr/bin/tr -d '*' | /usr/bin/grep -vE 'GTFOBins|Star|logo|GTFOBins|securit>
+                echo -e "\n${green}[¿?] If one or both fields appear without any results, it means that you don't have exploitable binaries or capabilities that appear in 'gtfobins', that's a good sign for your security ;)${end}\n"
+                echo -e "\n${blue}[*] Binaries exploitable:${end}"
+                for binary in $(curl -s -X GET "https://gtfobins.github.io/" | /usr/bin/html2text | /usr/bin/awk '{print $1}' | /usr/bin/tr -d '*' | /usr/bin/grep -vE 'GTFOBins|Star|logo|GTFOBins|secus|security|The|to|privileges|other|It|listed|how|GTFOBins|where|If|Binary|No' | /usr/bin/tr -d '[' | /usr/bin/tr -s '\n' | grep -E "$filterBinary")
                 do
-                        echo -e "\n${red}[!] $data ${end} -> ${yellow}https://gtfobins.github.io/gtfobins/$data${end}"
+                        echo -e "\n${red}[!] $binary ${end} -> ${yellow}https://gtfobins.github.io/gtfobins/$binary${end}"
+                done
+                echo -e "\n\n${blue}[*] Capabilities exploitable:${end}"
+                for capabilitie in $(/usr/bin/curl -s -X GET "https://gtfobins.github.io/#+capabilities" | /usr/bin/html2text | /usr/bin/grep -n "Capabilities" -B 5 | /usr/bin/awk '{print $1}' | /usr/bin/awk -F '-' '{print $NF}' | /usr/bin/grep -v ':' | /usr/bin/tr -s '\n' | /usr/bin/grep -E "$filterCapabilitie")
+                do
+                        echo -e "\n${red}[!] $capabilitie ${end} -> ${yellow}https://gtfobins.github.io/gtfobins/$capabilitie${end}"
                 done
 fi
